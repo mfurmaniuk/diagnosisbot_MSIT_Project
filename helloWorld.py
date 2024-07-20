30722ff80ff7da2957f50d3d71a7eb99bb84c845
Michael <mfurmaniuk@users.noreply.github.com>
2024-07-20 13:53:23 -0400

    Updated work for Week 2 submission

---
+++ helloWorld.py
@@ -0,0 +1,8 @@
+# Python Hello World program
+
+# Get the Users Name
+yourName = input("What is your name? ")
+
+# With the name, print out a personalized Hello World
+print("Greetings, {0}!".format(yourName))
+print("Just want to say - Hello, world!")
